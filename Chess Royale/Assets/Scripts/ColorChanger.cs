f0f6ec5470d6bfa6f0901d58c8410c2aa4b27d53
DavidYeLuo <dayeluo@galileoweb.org>
2019-04-13 17:10:15 -0700

    Added a selection layer

---
+++ Chess Royale/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public Material transparentYellow;
+    public Material transparentBlue;
+
+    private bool blue;
+    MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void SetToBlue()
+    {
+        meshRenderer.material.SetColor("_Color", transparentBlue.color);
+    }
+
+    public void SetToYellow()
+    {
+        meshRenderer.material.SetColor("_Color", transparentYellow.color);
+    }
+}
