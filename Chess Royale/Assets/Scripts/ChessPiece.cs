a31ab339a18a90655a56f06428b3c9697c561c88
DavidYeLuo <dayeluo@galileoweb.org>
2019-05-06 14:17:25 -0700

    Documentation Patch 1.0
    ChessPiece class

--- Chess Royale/Assets/Scripts/ChessPiece.cs
+++ Chess Royale/Assets/Scripts/ChessPiece.cs
@@ -6,10 +6,20 @@ using UnityEngine;
  * Steps on making a new chess piece class
  * 1) Your new class must inherit from this class.
  * 2) Override Start() 
- *  I) Your first line should include base.Start(); (so that pieces starts based on gameObject.transform).
- *  II) Override name with your desired name (Will need this to identify pieces).
- *  III) Add locations of where your piece can move in Vector2Int.
- * 3) To Add special moves specific to your chess piece, add a new switch case in GeneratePossibleMoves(ChessPiece).
+ *  I) Your first line should include base.Start(); (so that pieces starts based on gameObject.transform) (Might not need to do this in the future).
+ *  II) Override name with your desired name (Used to identify pieces but probably not needed anymore).
+ *  III) Add locations of where your piece can move in Vector2Int (Might not need to do this in the future).
+ *  IV) Override GeneratePossibleMoves(ChessBoard) with how you want pieces to move.
+ *  V) Override Move(Vector2Int) if desired
+ *  
+ *  Things to consider changing in this class:
+ *  The variable name is no longer used. Maybe in the future. (Considering removing this variable)
+ *  The variable moves isn't really used that much unless it's unrestricted movement like the horse. (Considering removing this variable)
+ *      Reason to remove this variable is due to many of the pieces only use generate possible moves which is independent from the list<Vector2Int> moves.
+ *      As a result, if we change the list of moves, it really doesn't change the moves.
+ *      Options to solve this problem:
+ *      remove the variable moves: (Easiest)
+ *      Smartly involve moves in the GeneratePossibleMoves methods for every piece
  */
 
 public abstract class ChessPiece : MonoBehaviour
